1d612d1a015737f21e3ca68de8372b4dc3949190
lisi <lisi@qq.com>
2024-11-18 11:17:37 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, loadEnv,ConfigEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path'
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }: ConfigEnv)=>{
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(()=>{
+  // const env = loadEnv(mode, process.cwd());
   return {
     plugins: [vue()],
     // server: {
